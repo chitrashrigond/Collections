71517e8122460a1d5ecfb34e4b0363a20612f273
Chitrashri <chitrashri.xworkz@gmail.com>
2021-07-16 21:29:32 +0530

    Task

---
+++ DataStore/src/com/xworkz/datastore/dao/RiverDAOImpl.java
@@ -0,0 +1,11 @@
+package com.xworkz.datastore.dao;
+import com.xworkz.datastore.dao.RiverDAO;
+public class RiverDAOImpl implements RiverDAO{
+
+	@Override
+	public boolean save(RiverDTO dto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
